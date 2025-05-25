5009c90f16d27a9298ab27fc9dbc68faf0d0adfa
VIRENDRA BADGOTYA <149055296+vir123-devf@users.noreply.github.com>
2025-05-25 21:10:38 +0530

    Add files via upload

---
+++ Challenge-2/src/preprocessing.py
@@ -0,0 +1,14 @@
+"""
+
+Author: Annam.ai IIT Ropar
+Team Name: SoilClassifiers
+Team Members: Member-1, Member-2, Member-3, Member-4, Member-5
+Leaderboard Rank: <Your Rank>
+
+"""
+
+# Here you add all the preprocessing related details for the task completed from Kaggle.
+
+def preprocessing():
+    print("This is the file for preprocessing")
+  return 0
