90fbfb17ad2a6f947fd1408bc0cf2b2628ad5dcc
Madhur Thareja <145339182+madhurthareja@users.noreply.github.com>
2025-05-25 08:34:25 +0530

    Create postprocessing.py

---
+++ challenge-1/src/postprocessing.py
@@ -0,0 +1,14 @@
+"""
+
+Author: Annam.ai IIT Ropar
+Team Name: SoilClassifiers
+Team Members: Member-1, Member-2, Member-3, Member-4, Member-5
+Leaderboard Rank: <Your Rank>
+
+"""
+
+# Here you add all the post-processing related details for the task completed from Kaggle.
+
+def postprocessing():
+    print("This is the file for postprocessing")
+  return 0
